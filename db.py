249e08e5209bccb202c91fa0c1802e100bb5a8f7
pmathiesoncricket <pmathiesoncricket@gmail.com>
2026-08-02 23:50:50 +1000

    Update db.py

--- db.py
+++ db.py
@@ -100,7 +100,8 @@ def fetch_all_rows(table_name: str, select_str: str, eq_filters: dict | None = N
 def get_matches():
     return fetch_all_rows(
         "matches",
-        "match_id, grade, matchtype, day1start, hometeamid, hometeam, awayteamid, awayteam, organisation_id, competition_id, competition_name",
+        "match_id, grade, match_type, day_1_start, home_team_id, home_team, "
+        "away_team_id, away_team, organisation_id, competition_id, competition_name",
         order_col="match_id",
     )
 
