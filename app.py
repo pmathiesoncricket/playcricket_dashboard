3b7f8add68e9ef1422550232a71e2cc6be61cd1b
pmathiesoncricket <pmathiesoncricket@gmail.com>
2026-08-05 17:30:39 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from tab_batting import batting_tab
 from tab_batting_season import batting_season_tab
 from tab_bowling import bowling_tab
 from tab_bowler_style import bowler_style_tab
+from tab_match_summary import match_summary_tab
 from tab_team import team_tab
 
 st.set_page_config(page_title="PlayCricket Dashboard", layout="wide")
 st.title("PlayCricket Dashboard")
 
-tabs = st.tabs(["Batting", "Batting Season Report", "Bowling", "Bowler Style", "Team"])
+tabs = st.tabs(["Batting", "Batting Season Report", "Bowling", "Bowler Style", "Match Summary", "Team"])
 
 with tabs[0]:
     batting_tab()
@@ -20,4 +21,6 @@ with tabs[2]:
 with tabs[3]:
     bowler_style_tab()
 with tabs[4]:
+    match_summary_tab()
+with tabs[5]:
     team_tab()
